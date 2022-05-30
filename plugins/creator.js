78c92df47e5b6b9b44dc901b76d9f2e7c2a8d99e
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-05-30 17:47:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,29 +5,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     contacts: [{
 
 
-"displayName": "CREATOR•SLIME",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:CREATOR•SLIME\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
- }, {
-   "displayName": "BABI",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:BABI\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "ANAK ANJINK",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK ANJINK\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "ANJINK",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANJINK\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "YATIM",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:YATIM\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "KONTOL",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:KONTOL\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "ANAK KONTOL",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ANAK KONTOL\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
-  }, {
-   "displayName": "ADRI",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ADRI\nitem1.TEL;waid=6281354079196:6281354079196\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rimuru418@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Adri158\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Tokyo 🇯🇵\nitem5.X-ABLabel:───────[ SLIMEBOT ]───────\nEND:VCARD"
+"displayName": "Fachri",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:CREATOR•FACHRIBOTZ\nitem1.TEL;waid=6285713041886:6285713041886\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:saefulfachri18@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:bit.ly/sfdesignblogspot\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇯🇵 Jepang;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia 🇮🇩\nitem5.X-ABLabel:───────[ FACHRIBOTZ ]───────\nEND:VCARD"
   }, {
   }, {
     }]
